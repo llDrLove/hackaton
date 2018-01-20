533ebb0550a018932ead839890e7c0cab9969c1b
Oliver Love <oliver.love.34@gmail.com>
2018-01-20 14:47:50 -0500

    Login api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\AuthRequest;
+use App\Algorithms\Helpers\HttpHelper;
 
 class AuthController extends Controller
 {
@@ -13,9 +16,23 @@ class AuthController extends Controller
     public function index(AuthRequest $request)
     {
     	try {
-    			
+    		if (Auth::attempt(['email' => $request['email'], 'password' => $request['password']])) {
+    			return HttpHelper::json([
+    				'userId' => Auth::user()
+    			]);
+    		} else {
+    			$user = new User();
+    			$user->name = $request['name'];
+    			$user->email = $request['email'];
+    			$user->password = bcrypt($request['password']);
+    			$user->saveOrFail();
+    			return HttpHelper::json([
+    				'userId' => $user->id,
+    				'message' => 'Account created !'
+    			], 200);
+    		}	
 		} catch (Exception $e) {
-			
+			return HttpHelper::json(['message' => 'An error occured!'], 500);
 		}	
     }
 }
--- app/Http/Requests/AuthRequest.php
+++ app/Http/Requests/AuthRequest.php
@@ -24,6 +24,7 @@ class AuthRequest extends FormRequest
     public function rules()
     {
         return [
+            'name'     => 'required|string|max:255',
             'email'    => 'required|email|max:255',
             'password' => 'required|min:6',
         ];
